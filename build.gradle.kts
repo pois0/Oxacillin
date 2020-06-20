cc2c7afc8e1e21e47de6e61a5e15f27e458cbb5c
Nep <spica@starry.blue>
2020-06-20 17:44:14 +0900

    Workaround for Ktor & Kotlin DCE issues
    Signed-off-by: Nep <spica@starry.blue>

--- build.gradle.kts
+++ build.gradle.kts
@@ -121,8 +121,13 @@ kotlin {
         browser {
             testTask {
                 useKarma {
-                    useChromeHeadless()
+                    useChrome()
                 }
+            }
+
+            dceTask {
+                // workaround for https://kotlinlang.org/docs/reference/javascript-dce.html#known-issue-dce-and-ktor.
+                keep("ktor-ktor-io.\$\$importsForInline\$\$.ktor-ktor-io.io.ktor.utils.io")
             }
         }
     }
